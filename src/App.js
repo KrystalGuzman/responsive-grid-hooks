ae54d6bc388aa45b6a56f6e5c3204f2d2d3baffe
Krystal Guzman <kryguzman@gmail.com>
2020-04-12 16:48:22 -0700

    finished Material-UI tutorial with different API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import PplCard from "./PplCard";
+import {Grid} from '@material-ui/core';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [users, setUsers] = useState([]);
+
+    useEffect(()=>{
+        async function fetchData(){
+            setUsers(
+                await fetch('https://randomuser.me/api/?results=10')
+                .then(res=> res.json())
+                // .then(res=>console.log(res.results))
+                .then(res=>res.results)
+                .catch(err => console.log(err, "Fetching error"))
+            )
+        }
+        fetchData();
+      }
+    ,[])
+    return (
+        <div className="App">
+            <h3>The True Beauty of Material-UI</h3>
+            <Grid container spacing={10} styled={{padding: '24px'}}>
+              {/* {users.map(user => user.email)} */}
+              {users.map(user => 
+                <Grid key={user.id} item xs={12} sm={6} md={4} lg={4} xl={3}>
+              <PplCard 
+              key={user.id}
+              email={user.email}
+              firstname={user.name.first}
+              lastname={user.name.last}
+              picture={user.picture.medium} 
+              />
+            </Grid>
+            )}
+            </Grid>
+            
+        </div>
+    );
 }
 
 export default App;
