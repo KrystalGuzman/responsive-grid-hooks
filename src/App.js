7682ac2fc085c0b1ceb5e0e7ea4b3219e40db656
Krystal Guzman <kryguzman@gmail.com>
2020-08-05 08:51:02 -0700

    updates app.js and pplcard

--- src/App.js
+++ src/App.js
@@ -1,44 +1,67 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import PplCard from "./PplCard";
-import {Grid} from '@material-ui/core';
+import PplCard from './PplCard';
+import { Grid } from '@material-ui/core';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
 
 function App() {
-    const [users, setUsers] = useState([]);
+	const [ users, setUsers ] = useState([]);
+	const [ value, setValue ] = React.useState('female');
 
-    useEffect(()=>{
-        async function fetchData(){
-            setUsers(
-                await fetch('https://randomuser.me/api/?results=10')
-                .then(res=> res.json())
-                // .then(res=>console.log(res.results))
-                .then(res=>res.results)
-                .catch(err => console.log(err, "Fetching error"))
-            )
-        }
-        fetchData();
-      }
-    ,[])
-    return (
-        <div className="App">
-            <h3>The True Beauty of Material-UI</h3>
-            <Grid container spacing={10} styled={{padding: '24px'}}>
-              {/* {users.map(user => user.email)} */}
-              {users.map(user => 
-                <Grid key={user.id} item xs={12} sm={6} md={4} lg={4} xl={3}>
-              <PplCard 
-              key={user.id}
-              email={user.email}
-              firstname={user.name.first}
-              lastname={user.name.last}
-              picture={user.picture.medium} 
-              />
-            </Grid>
-            )}
-            </Grid>
-            
-        </div>
-    );
+	const handleChange = (event) => {
+		setValue(event.target.value);
+	};
+
+	useEffect(() => {
+		async function fetchData() {
+			setUsers(
+				await fetch('https://randomuser.me/api/?results=10')
+					.then((res) => res.json())
+					// .then(res=>console.log(res.results))
+					.then((res) => res.results)
+					.catch((err) => console.log(err, 'Fetching error'))
+			);
+		}
+		fetchData();
+	}, []);
+
+	return (
+		<div className="App">
+      
+			<h3>The True Beauty of Dating</h3>
+
+{/* Gender */}
+			<FormControl component="fieldset">
+				<FormLabel component="legend">Gender</FormLabel>
+				<RadioGroup aria-label="gender" name="gender1" value={value} onChange={handleChange}>
+					<FormControlLabel value="female" control={<Radio />} label="Female" />
+					<FormControlLabel value="male" control={<Radio />} label="Male" />
+				</RadioGroup>
+			</FormControl>
+
+			<Grid container spacing={10} styled={{ padding: '24px' }}>
+				{users.filter((gen) => gen.gender === value).map((user) => (
+					<Grid key={user.id} item xs={12} sm={6} md={4} lg={4} xl={3}>
+						<PplCard
+							key={user.id}
+							city={user.location.city}
+							state={user.location.state}
+							age={user.dob.age}
+							email={user.email}
+							firstname={user.name.first}
+							lastname={user.name.last}
+							picture={user.picture.large}
+							phone={user.phone}
+						/>
+					</Grid>
+				))}
+			</Grid>
+		</div>
+	);
 }
 
 export default App;
--- src/PplCard.js
+++ src/PplCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Description from "./Description";
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -8,19 +9,28 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
+
 const useStyles = makeStyles({
   root: {
     maxWidth: 345,
+    margin: "auto",
   },
   media: {
-    height: 140,
+    height: 100,
+    width: 100,
+    borderRadius: 50,
+    margin: "auto",
   },
 });
 
-export default function PplCard({email, firstname, lastname, picture}) {
+export default function PplCard({phone, city, state, age, email, firstname, lastname, picture}) {
   const classes = useStyles();
-console.log(email)
+
+  const [showMore, setShowMore] = React.useState(false)
+  const onClick = () => setShowMore(true)
+
   return (
+    <div>
     <Card className={classes.root}>
       <CardActionArea>
         <CardMedia
@@ -32,18 +42,28 @@ console.log(email)
             {firstname +" "+ lastname}
           </Typography>
           <Typography variant="body2" color="textSecondary" component="p">
-            {email}
+            {city}, {state}
           </Typography>
+          <div id="learnMore">
+        <Typography variant="body2" color="textSecondary" component="p">
+            Age: {age}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Email: {email}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Phone: {phone}
+          </Typography>
+        </div>
         </CardContent>
       </CardActionArea>
       <CardActions>
-        <Button size="small" color="primary">
-          Share
+        <Button size="small" color="primary" onClick={onClick}>
+          { showMore ? <Description /> : "Learn More" }
         </Button>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
+        
       </CardActions>
     </Card>
+    </div>
   );
 }
